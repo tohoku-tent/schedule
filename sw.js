62a6876450e42a7cf3a8bde62453e59713a3a7e2
tohoku-tent <love.child1105@gmail.com>
2026-05-12 14:40:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'tohoku-tent-schedule-v1';
+const URLS_TO_CACHE = [
+  '/schedule/schedule.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
